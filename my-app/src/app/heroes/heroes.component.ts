f9af803843ed287d635ec7f820b797cfe8dc6790
ni8mr <nilpaddo@gmail.com>
2021-03-24 09:22:36 +0600

    In the middle of implementing service architecture.

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -13,7 +13,7 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes()
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
   }
 
   // Injecting HeroService
