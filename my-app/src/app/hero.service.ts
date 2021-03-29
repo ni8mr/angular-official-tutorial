35fe6fce50614bc3c9f7d2948bf716154b0fcff2
ni8mr <nilpaddo@gmail.com>
2021-03-29 07:47:16 +0600

    Save the last state, in the middle of implementing http client.

--- my-app/src/app/hero.service.ts
+++ my-app/src/app/hero.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from "rxjs/observable/of";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from "./message.service";
 
 @Injectable()
 export class HeroService {
+  private heroUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   getHeroes():  Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroUrl)
   }
 
   getHero(id: Number): Observable<Hero> {
@@ -19,6 +25,9 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id));
   }
 
-  constructor(private messageService: MessageService) { }
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
 
 }
